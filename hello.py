e46d82c03718f77d4b8ed32910f5ee0f09b2f70f
krustea <43367425+krustea@users.noreply.github.com>
2019-01-31 16:46:04 +0100

    Update hello.py

--- hello.py
+++ hello.py
@@ -12,16 +12,16 @@ def hello():
 @app.route('/on/<nbr>')
 def on(nbr):
     if nbr == '1':
-        led1.on()
+        led1.on(14)
     elif nbr == '2':
-        led2.on()
+        led2.on(15)
     return render_template('on.html', nbr=nbr)
 
 @app.route('/off/')
 @app.route('/off/<nbr>')
 def off(nbr):
     if nbr == '1':
-        led1.off()
+        led1.off(14)
     elif nbr == '2':
-        led2.off()
+        led2.off(15)
     return render_template('on.html', nbr=nbr)
