71c2b1ea446a20bd038969743dad280a7a984fc0
krustea <43367425+krustea@users.noreply.github.com>
2019-01-31 16:05:21 +0100

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,6 +1,20 @@
 from flask import Flask
 app = Flask(__name__)
 
+class Led:
+    def __init__(self, numero_broche, couleur):
+        self.numero_broche = numero_broche
+        self.couleur = couleur
+
+    def on(self, numero_broche):
+        return GPIO.output(numero_broche, GPIO.HIGH)
+
+    def off(self, numero_broche):
+        return GPIO.output(numero_broche, GPIO.LOW)
+
+led1= Led('14', 'rouge')
+led2= Led('15', "bleu")
+
 from flask import render_template
 @app.route('/on/')
 def hello():
@@ -19,20 +33,21 @@ GPIO.setup(14, GPIO.OUT)
 GPIO.setup(15, GPIO.OUT)
 
 
+
 @app.route('/on/')
 @app.route('/on/<nbr>')
 def on(nbr):
     if nbr == '1':
-        GPIO.output(14, GPIO.HIGH)
+        led1.on
     elif nbr == '2':
-        GPIO.output(15, GPIO.HIGH)
+        led2.on
     return render_template('on.html', nbr=nbr)
 
 @app.route('/off/')
 @app.route('/off/<nbr>')
 def off(nbr):
     if nbr == '1':
-        GPIO.output(14, GPIO.LOW)
+        led1.off
     elif nbr == '2':
-        GPIO.output(15, GPIO.LOW)
+        led2.off
     return render_template('on.html', nbr=nbr)
