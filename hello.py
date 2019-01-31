7af026ea366ac489c6072db9658de6bb3ef137c3
krustea <43367425+krustea@users.noreply.github.com>
2019-01-31 16:24:49 +0100

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,3 +1,5 @@
+from ledobject import Led
+
 from flask import Flask
 app = Flask(__name__)
 
@@ -20,34 +22,20 @@ from flask import render_template
 def hello():
     return render_template('on.html')
 
-import RPi.GPIO as GPIO
-import time
-
-#Utilisation d'une norme de nommage pour les broches
-GPIO.setmode(GPIO.BCM)
-GPIO.setwarnings(False)
-
-#initialisation de la broche en mode "sortie"
-#⚠️ Le nombre passé en paramètre correspond au numéro de GPIO et non au numéro de la broche.
-GPIO.setup(14, GPIO.OUT)
-GPIO.setup(15, GPIO.OUT)
-
-
-
 @app.route('/on/')
 @app.route('/on/<nbr>')
 def on(nbr):
     if nbr == '1':
-        led1.on
+        led1.on()
     elif nbr == '2':
-        led2.on
+        led2.on()
     return render_template('on.html', nbr=nbr)
 
 @app.route('/off/')
 @app.route('/off/<nbr>')
 def off(nbr):
     if nbr == '1':
-        led1.off
+        led1.off()
     elif nbr == '2':
-        led2.off
+        led2.off()
     return render_template('on.html', nbr=nbr)
