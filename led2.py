213374adcc7a36e736694ff5d903d750d792dc98
krustea <43367425+krustea@users.noreply.github.com>
2019-02-01 14:37:01 +0100

    Update led2.py

--- led2.py
+++ led2.py
@@ -1,4 +1,5 @@
 import RPi.GPIO as GPIO
+import time
 class Led2:
     def __init__(self):
         GPIO.setmode(GPIO.BCM)
@@ -15,6 +16,15 @@ class Led2:
             GPIO.output(24, GPIO.LOW)
             GPIO.output(22, GPIO.LOW)
         elif (Celsius > 30):
-            GPIO.output(24, GPIO.HIGH)
-            GPIO.output(22, GPIO.HIGH)
+                i=0
+                while i < 100:
+                    GPIO.output(24, GPIO.HIGH)
+                    GPIO.output(22, GPIO.HIGH)
+                    time.sleep(1)
+                    GPIO.output(24, GPIO.LOW)
+                    GPIO.output(22, GPIO.LOW)
+                    time.sleep(1)
+                    i=i+1
+            
+            
             
