ae6e9f83e071f2851867cff0f92ca896c7414264
krustea <43367425+krustea@users.noreply.github.com>
2019-02-01 14:25:41 +0100

    Update led2.py

--- led2.py
+++ led2.py
@@ -9,6 +9,10 @@ class Led2:
     def led2_Celsius(self, Celsius):
         if (Celsius < 15):
             GPIO.output(18, GPIO.HIGH)
+        elif(Celsius >= 15 and Celsius <= 30):
+            GPIO.output(18, GPIO.LOW)
+            GPIO.output(24, GPIO.LOW)
         elif (Celsius > 30):
             GPIO.output(24, GPIO.HIGH)
+            GPIO.output(22, GPIO.HIGH)
             
